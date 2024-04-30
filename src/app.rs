3cb4fb08aa5e71892c51e526f41faa26a738eea4
jmatte <joseph1matteson@gmail.com>
2024-04-29 21:54:53 -0500

    first two UI inputs

--- src/app.rs
+++ src/app.rs
@@ -1,35 +1,47 @@
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use ratatui::widgets;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Widget {
+  Topic,
+  GroupId,
+}
 
 #[derive(Debug)]
-pub enum ActiveWidget { Topic, GroupId}
+pub enum Mode {
+  Normal,
+  Editing,
+}
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct App {
-  pub active_widget: Option<ActiveWidget>,
+  pub widgets: [Widget; 2],
+  pub active_widget: Option<Widget>,
+  pub mode: Mode,
   pub should_quit: bool,
 }
 
 impl App {
   pub fn new() -> Self {
     App {
+      widgets: [Widget::Topic, Widget::GroupId],
       active_widget: None,
+      mode: Mode::Editing,
       should_quit: false,
     }
   }
 
-  pub fn tick(&self) {}
-
   pub fn update(&mut self, key_event: KeyEvent) {
     match key_event.code {
       KeyCode::Esc | KeyCode::Char('q') => self.should_quit = true,
-      KeyCode::Char('h') => {
+      KeyCode::Char('j') => {
         if key_event.modifiers == KeyModifiers::CONTROL {
-          self.should_quit = true
+          self.active_widget = Some(self.widgets[0])
         }
       }
-      KeyCode::Char('l') => {
+      KeyCode::Char('k') => {
         if key_event.modifiers == KeyModifiers::CONTROL {
-          self.should_quit = true
+          self.active_widget = Some(self.widgets[1])
         }
       }
       // KeyCode::Right | KeyCode::Char('j') => self.counter += 1,
--- src/ui.rs
+++ src/ui.rs
@@ -1,25 +1,52 @@
 use color_eyre::owo_colors::{style, OwoColorize};
 use ratatui::{
-  layout::{Constraint, Direction},
+  layout::{Constraint, Direction, Rect},
   prelude::{Alignment, Frame, Layout},
   style::{Color, Style},
   widgets::{Block, BorderType, Borders, Paragraph},
 };
 
-use crate::app::App;
+use crate::app::{App, Widget};
 
 pub fn render(app: &mut App, f: &mut Frame) {
-  let parent_layout = Layout::default()
-    .constraints(vec![Constraint::Percentage(100)])
-    .split(f.size());
+  let sub_layout = Layout::default()
+    .margin(1)
+    .constraints(vec![
+      Constraint::Percentage(20),
+      Constraint::Percentage(40),
+      Constraint::Percentage(40),
+    ])
+    .split(Rect::new(0, 0, f.size().width, 10));
 
-  let block = Block::default()
-    .title("Consumers")
-    .title_alignment(Alignment::Center)
+  let help = Paragraph::new(String::from("Press ^j and ^k to move up and down"))
+    .block(Block::default().borders(Borders::NONE));
+
+  let topic = Block::default()
+    .title("topic")
+    .title_alignment(Alignment::Left)
     .borders(Borders::ALL)
-    .style(Style::default().fg(Color::Cyan));
+    .style(Style::default().fg(Color::Gray));
 
-  f.render_widget(block, parent_layout[0]);
+  let group_id = Block::default()
+    .title("group id")
+    .title_alignment(Alignment::Left)
+    .borders(Borders::ALL)
+    .style(Style::default().fg(Color::Gray));
+
+  // if let Some(widget) = app.active_widget {
+  //   match widget {
+  //     Widget::Topic => {
+  //       &topic.style(Style::default().fg(Color::Cyan));
+  //     }
+  //     Widget::GroupId => {
+  //       &group_id.style(Style::default().fg(Color::Cyan));
+  //     }
+  //   }
+  // }
+
+  f.render_widget(help, sub_layout[0]);
+  f.render_widget(topic, sub_layout[1]);
+  f.render_widget(group_id, sub_layout[2]);
 
   // f.render_widget(
   //   Paragraph::new(format!(
